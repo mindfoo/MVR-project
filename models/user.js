44f9bf130957089cdf431420b1bbd96f01a6fa7d
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-05-02 16:00:21 +0100

    user model added

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		firstName: String,
+		lastName: String,
+		username: String,
+		email: String,
+		password: String,
+		playlist: [{ type: Schema.Types.ObjectId, ref: "playlist" }],
+	},
+	{
+		timestamps: {
+			createdAt: "created_at",
+			updatedAt: "updated_at",
+		},
+	}
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = Playlist;
