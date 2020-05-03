d3a89358ee5d0fa90bb472beffb9a216fccafeb6
Margarida Pascoal <amppascoal@gmail.com>
2020-05-03 17:46:30 +0100

    adding env configs and changing view and create route

--- app.js
+++ app.js
@@ -9,27 +9,13 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
 //using bootstrap in a cool way
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-
-
-// require spotify-web-api-node package here:
-const SpotifyWebApi = require('spotify-web-api-node');
-
-// 1. Setting the spotify-api goes here:
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET
-});
-
-// 2. Retrieve an access token
-spotifyApi
-.clientCredentialsGrant()
-.then(data => spotifyApi.setAccessToken(data.body['access_token']))
-.catch(error => console.log('Something went wrong when retrieving an access token', error));
-
-
-
 
 mongoose
   .connect('mongodb://localhost/mvr-project', {useNewUrlParser: true})
@@ -40,10 +26,7 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
