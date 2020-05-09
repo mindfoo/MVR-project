474a8d2b37e70829901906904ad5f5b6cf3a046a
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-05-09 12:10:17 +0100

    debugging

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const User = require("../models/user");
-const Song = require("../models/song");
+// const express = require("express");
+// const User = require("../models/user");
+// const Song = require("../models/song");
 
-const router = express.Router();
+// const router = express.Router();
 
-//profile route
-router.get("/profile", (req, res, next) => {
-	try {
-		res.render("profile/profile");
-	} catch (e) {
-		next(e);
-	}
-});
+// //profile route
+// router.get("/profile", (req, res, next) => {
+// 	try {
+// 		res.render("profile/profile");
+// 	} catch (e) {
+// 		next(e);
+// 	}
+// });
 
 
-module.exports = router;
+// module.exports = router;
