ac348cc824c2c22ebabd418a8bb618f5a6307030
Margarida Pascoal <amppascoal@gmail.com>
2020-05-09 16:45:41 +0100

    playlist update with andre help

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Song = require("../models/song");
+const Playlist = require("../models/playlist");
 
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
@@ -19,28 +20,25 @@ spotifyApi
 		console.log("Something went wrong when retrieving an access token", error)
 	);
 
-
-
-
 let artistname; //  we need the artist name (artname) in the router.post (/add-playlist) so we are making it global.
 // It is declared here and initialized in router.get(/artist-search)
 
 // Our routes go here:
 router.get("/", (req, res, next) => {
 	const currentUser = req.session.currentUser;
-	res.render("index", {currentUser});
+	res.render("index", { currentUser });
 });
 
 
 // CHECK if the user is logged in and send to secret
-router.use((req,res,next) => {
+router.use((req, res, next) => {
 	console.log(req.session.currentUser)
 	if (req.session.currentUser) {
-	  next();
+		next();
 	} else {
-	  res.redirect('/login');
+		res.redirect('/login');
 	}
-  })
+})
 
 // 1. Search artists and choose artist
 
@@ -80,6 +78,9 @@ router.get("/tracks/:id", (req, res) => {
 			// console.log('The received data from the API: ', data.body.images);
 			// console.log('One of the items of the data: ', data.body.artists.items[0])});
 			let items = data.body.items;
+			console.log('DATA     ', data.body.items[0].artists[0].name)
+
+			artist_name = data.body.items[0].artists[0].name;
 			// console.log(items)
 			//  items.forEach(element => console.log(element.id))
 			let albumsIds = [];
@@ -97,22 +98,25 @@ router.get("/tracks/:id", (req, res) => {
 					items.forEach((element) => allTracks.push(element.name));
 					items.forEach((element) => allPreview_url.push(element.preview_url));
 
-					console.log(`This is allTracks logging from INSIDE the forEach`);
-					console.log(allTracks);
+					//console.log(`This is allTracks logging from INSIDE the forEach`);
+					//console.log('ALL TRA|CKS',allTracks);
 					if (counter === albumsIds.length) {
 						// pq o problema q tava a dar com artistas com mais de 1 album parecia estar relacionado com o res.render estar a ser chamado tantas vezes quantas o número de albums
 						allTracks = new Set(allTracks); // remove os duplicados
 						allInfo.name = allTracks;
 						allInfo.preview = allPreview_url;
-						res.render("playlist/all-tracks", { allInfo });
+						//console.log(allInfo);
+						let data = { allInfo, artist_name };
+						console.log('MAYBE', data)
+						res.render("playlist/all-tracks", { data });
 					}
-					console.log(counter);
-					console.log(albumsIds.length);
+					//console.log(counter);
+					//console.log(albumsIds.length);
 					counter = counter + 1;
 				})
 			);
-			console.log(`This is allTracks logging from OUTSIDE the forEach`);
-			console.log(allTracks);
+			//console.log(`This is allTracks logging from OUTSIDE the forEach`);
+			//console.log(allTracks);
 			//  res.render('all-tracks', { allInfo }  )
 		})
 		.catch((err) =>
@@ -122,39 +126,44 @@ router.get("/tracks/:id", (req, res) => {
 
 // 3. POST chosen songs to Database
 
-router.post('/add-playlist', (req, res) => {
-  console.log("I am on the add-playlist route")
-  console.log(artistname)
-  const songs = req.body.song; 
-  console.log(songs)
+router.post('/add-playlist', (req, res, next) => {
 
+	console.log("I am on the add-playlist route")
+	console.log(artist_name)
+	const songs = req.body.song;
+	console.log("REQ BODY: ", req.body.artist_name)
+	let newSongsId = [];
+	artistname = req.body.artist_name;
 
-  let newSongsId = [];
-  for (let i = 0; i < songs.length; i++) {
-    songName = songs[i]
-    artistname = artistname;
-    let newSong = new Song({songName, artistname});
-    newSong.save()
-    .then( () => {
-      console.log(newSong._id) 
-      newSongsId.push(newSong._id)   
-      console.log(newSongsId) 
-      console.log("A new song was saved in the DB")
-      if ( i + 1 === songs.length) {
-        res.redirect('/') //  --> Se não utilizar o if dá o erro "Cannot set headers after they are sent to the client" apesar de gravar na DB
-     // deve ser o mesmo problema que acontecia em albumsId.forEach().
-      }
-    })
-    .catch((error) => {
-            console.log(error);
-    }) 
-  }
+	for (let i = 0; i < songs.length; i++) {
+		songName = songs[i];
+		//newSongsId.push(newSong._id)
+		let newSong = new Song({ songName, artistname });
 
-    // let newPlaylist = new Playlist ({artistName, songs, user})
-    // newPlaylist.save()
-    // .then()
+		newSong.save()
+			.then((result) => {
+				console.log(result)
+				newSongsId.push(result._id)
+				//console.log(newSongsId) 
+				console.log("A new song was saved in the DB")
+			})
+	}
 
+	//console.log(newSongsId) 
+	//console.log("A new song was saved in the DB")
+	let user = req.session.currentUser._id;
+	const newPlaylist = new Playlist({ artistname, songs: newSongsId, user })
+
+	newPlaylist.save()
+		.then(() => {
+			// if ( i + 1 === songs.length) {
+			res.redirect('/') //  --> Se não utilizar o if dá o erro "Cannot set headers after they are sent to the client" apesar de gravar na DB
+			// deve ser o mesmo problema que acontecia em albumsId.forEach().
+			// }
+		})
+		.catch((error) => {
+			next(error);
+		});
 })
-
 
 module.exports = router;
