f9ec01d91d021b73abf61bdcb3638d86d0c9dbdf
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-05-09 16:55:43 +0100

    profile edit 1

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,53 @@
-// const express = require("express");
-// const User = require("../models/user");
-// const Song = require("../models/song");
+const express = require("express");
+const User = require("../models/user");
 
-// const router = express.Router();
+const router = express.Router();
 
-// //profile route
-// router.get("/profile", (req, res, next) => {
-// 	try {
-// 		res.render("profile/profile");
-// 	} catch (e) {
-// 		next(e);
-// 	}
-// });
+//profile route
+router.get("/profile", (req, res, next) => {
+	try {
+		const currentUser = req.session.currentUser;
+		res.render("profile/profile", { currentUser });
+	} catch (e) {
+		next(e);
+	}
+});
 
+// CHECK if the user is logged in and send to secret
+router.use((req, res, next) => {
+	if (req.session.currentUser) {
+		next();
+	} else {
+		res.redirect("/login");
+	}
+});
 
-// module.exports = router;
+//user edit route
+router.get("/profile/edit", (req, res) => {
+	const userId = req.query.user_id;
+	User.findById(userId)
+		.then((theUser) => {
+			res.render("profile/profile-edit", { user: theUser });
+		})
+		.catch((error) => {
+			next(error);
+		});
+});
+
+//user edit post
+router.post("/profile/edit", (req, res) => {
+	const userId = req.query.user_id;
+	const { firstName, lastName, username, email } = req.body;
+	User.update(
+		{ _id: userId },
+		{ $set: { firstName, lastName, username, email } }
+	)
+		.then(() => {
+			res.redirect("/profile");
+		})
+		.catch((error) => {
+			next(error);
+		});
+});
+
+module.exports = router;
