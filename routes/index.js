933fd72e2f70a20c7376bab6eb44cec8a02fad43
Margarida Pascoal <amppascoal@gmail.com>
2020-05-13 22:58:31 +0100

    more styles and reorganizing playlist views

--- routes/index.js
+++ routes/index.js
@@ -57,11 +57,11 @@ router.use((req, res, next) => {
 });
 
 // GET Search for artists
-router.get("/artist-search", (req, res, next) => {
-	res.render("playlist/create-top");
+router.get("/artist-search", (req, res) => {
+	res.render("playlist/artist-search");
 });
 
-// GET Search for artists result
+// GET results for artists search
 router.get("/artist-search-action", (req, res, next) => {
 	//  console.log(req.query.artname) // --> { artname: 'placebo' } if in the form I type "placebo" and submit the form
 	artistname = req.query.artistname;
@@ -73,11 +73,11 @@ router.get("/artist-search-action", (req, res, next) => {
 			//  console.log('One of the items of the data: ', data.body.artists.items[0]);
 			let artists = data.body.artists.items;
 			//  console.log('sending data to artist-search results')
-			res.render("playlist/artist-search-results", { artists });
+			res.render("playlist/artist-search-results", { artists, artistname })
 			return artistname; // we need the artist name (artistname) in the router.post (/add-playlist)
 		})
 		.catch((err) =>
-			console.log("The error while searching artists occurred: ", err)
+			console.log("The error while searching artist occurred: ", err)
 		);
 });
 
